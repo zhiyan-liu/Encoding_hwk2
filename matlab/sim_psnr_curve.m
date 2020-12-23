cf55958fd4c110374009282b8d515928c3eaba51
johnzja <johnzja@qq.com>
2020-12-23 21:14:16 +0800

    ta's bugs fixed

--- matlab/sim_psnr_curve.m
+++ matlab/sim_psnr_curve.m
@@ -24,7 +24,7 @@ if infoSrcImage.ColorType == "truecolor"
 end
 
 %% Start simulations and record R-D curve.
-Ebn0 = 6;   % in dB.
+Ebn0 = 2;   % in dB.
 N_sim = 100;
 quant_step_arr = [4, 8, 10, 15, 20, 25, 30, 40];
 N_rates = length(quant_step_arr);
@@ -39,7 +39,7 @@ for r_idx = 1:N_rates
     [transmit_bitstream, codebook, height, width] = src_vlc(procImage, src_vlc_conf);
     psnr_arr = zeros(N_sim, 1);
     
-    for sim_idx = 1:N_sim
+    parfor sim_idx = 1:N_sim
         recv_bitstream = channel_transmit(transmit_bitstream, channel_conf, Ebn0);
         recImage = src_decode(recv_bitstream, codebook, height, width, src_vlc_conf);
         % isequal(procImage, recImage)
